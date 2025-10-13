7a33ee4db1fc1d90c90b66673024d63fe78427c4
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-14 08:05:56 +0900

    Laravel Breeze install時のrouteを復元

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Topページの場合、/quotにリダイレクト
@@ -12,5 +13,15 @@ Route::get('/quoot/create', App\Http\Controllers\Quoot\Create\CreateController::
 Route::get('/user/{userName}', App\Http\Controllers\User\UserController::class);
 Route::get('/user/{userName}/follows', App\Http\Controllers\User\FollowsController::class);
 Route::get('/user/{userName}/followers', App\Http\Controllers\User\FollowersController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
