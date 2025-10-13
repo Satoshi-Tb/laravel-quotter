57bc26122ea3b8b0de748e3f0ceccd79375fed19
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-14 00:27:10 +0900

    larabel breeze のroutes設定修復

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,3 +12,5 @@ Route::get('/quoot/create', App\Http\Controllers\Quoot\Create\CreateController::
 Route::get('/user/{userName}', App\Http\Controllers\User\UserController::class);
 Route::get('/user/{userName}/follows', App\Http\Controllers\User\FollowsController::class);
 Route::get('/user/{userName}/followers', App\Http\Controllers\User\FollowersController::class);
+
+require __DIR__.'/auth.php';
