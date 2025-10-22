a0ac33e5f936444f0d3b8c0397d247e2c54db724
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-22 23:43:46 +0900

    PHPStanエラー解消

--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -16,10 +16,10 @@ class UserController extends Controller
     {
         // userNameに基づいてQuserを取得する.ユーザー情報が取得できない場合、404エラーを返す
         $quser = Quser::where('user_name', $userName)->firstOrFail();
-        $quoots = Quoot::where('user_id', $quser->id)->get() ?? [];
+        $quoots = Quoot::where('user_id', $quser->id)->get();
         return view('user.index')->with([
             'userName' => $userName,
-            'displayName' => $quser ? $quser->display_name : '',
+            'displayName' => $quser->display_name,
             'quoots' => $quoots,
         ]);
     }
