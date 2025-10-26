5ffe5e1a130ff42d2bb0f4f6c6dc2ee07be650b4
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-26 14:24:27 +0900

    followテーブル移行

---
+++ backend/database/data/follows_insert.sql
@@ -0,0 +1,54 @@
+insert into follows (
+   following_user_id,
+   followed_user_id,
+   created_at,
+   updated_at
+) values ( 1,
+           2,
+           now(),
+           now() );
+insert into follows (
+   following_user_id,
+   followed_user_id,
+   created_at,
+   updated_at
+) values ( 1,
+           3,
+           now(),
+           now() );
+insert into follows (
+   following_user_id,
+   followed_user_id,
+   created_at,
+   updated_at
+) values ( 1,
+           4,
+           now(),
+           now() );
+insert into follows (
+   following_user_id,
+   followed_user_id,
+   created_at,
+   updated_at
+) values ( 2,
+           1,
+           now(),
+           now() );
+insert into follows (
+   following_user_id,
+   followed_user_id,
+   created_at,
+   updated_at
+) values ( 2,
+           4,
+           now(),
+           now() );
+insert into follows (
+   following_user_id,
+   followed_user_id,
+   created_at,
+   updated_at
+) values ( 3,
+           4,
+           now(),
+           now() );
