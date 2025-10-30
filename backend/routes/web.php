a5e945e7660a557e3573e6faacaff0b30f8996db
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-10-30 23:38:47 +0900

    web.phpを整理

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -8,22 +8,36 @@ Route::get('/', function () {
     return redirect('/quoot');
 });
 
-Route::get('/quoot', App\Http\Controllers\Quoot\IndexController::class)->name('quoot.index');
-Route::get('/quoot/create', App\Http\Controllers\Quoot\Create\CreateController::class)->middleware('auth');
-Route::post('/quoot/create', App\Http\Controllers\Quoot\Create\PostController::class)->middleware('auth');
-Route::get('/quoot/update/{quootId}', App\Http\Controllers\Quoot\Update\UpdateController::class)->middleware('auth');
-Route::put('/quoot/update/{quootId}', App\Http\Controllers\Quoot\Update\PutController::class)->middleware('auth');
-Route::delete('/quoot/delete/{quootId}', \App\Http\Controllers\Quoot\Delete\DeleteController::class)->middleware('auth')->name('quoot.delete');
-Route::get('/user/{userName}', App\Http\Controllers\User\UserController::class)->name('user.index');
-Route::post('/user/{userName}/follow', App\Http\Controllers\User\FollowAction\FollowUserController::class)->middleware('auth');
-Route::delete('/user/{userName}/follow', App\Http\Controllers\User\FollowAction\UnfollowUserController::class)->middleware('auth');
-Route::get('/user/{userName}/follows', App\Http\Controllers\User\FollowsController::class);
-Route::get('/user/{userName}/followers', App\Http\Controllers\User\FollowersController::class);
-Route::get('/chat/{chatId}', App\Http\Controllers\Chat\ChatController::class)->middleware('auth')->name('chat.index');
-Route::post('/chat/{chatId}/messages', App\Http\Controllers\Chat\MessagePostController::class)->middleware('auth');
-Route::post('/user/{userName}/chat', App\Http\Controllers\Chat\MakeChatRoomController::class)->middleware('auth');
-Route::get('/user/{userName}/edit', App\Http\Controllers\User\Edit\EditController::class)->middleware('auth')->name('user.edit');
-Route::put('/user/{userName}/edit', App\Http\Controllers\User\Edit\EditPutController::class)->middleware('auth')->name('user.edit.put');
+Route::prefix('/quoot')->group(function () {
+    Route::get('', \App\Http\Controllers\Quoot\IndexController::class)->name('quoot.index');
+});
+
+Route::prefix('/quoot')->middleware('auth')->group(function () {
+    Route::get('/create', \App\Http\Controllers\Quoot\Create\CreateController::class);
+    Route::post('/create', \App\Http\Controllers\Quoot\Create\PostController::class);
+    Route::get('/update/{quootId}', \App\Http\Controllers\Quoot\Update\UpdateController::class)->name('quoot.update');
+    Route::put('/update/{quootId}', \App\Http\Controllers\Quoot\Update\PutController::class)->name('quoot.update.put');
+    Route::delete('/delete/{quootId}', \App\Http\Controllers\Quoot\Delete\DeleteController::class)->name('quoot.delete');
+});
+
+Route::prefix('/chat')->middleware('auth')->group(function () {
+    Route::get('/{chatId}', \App\Http\Controllers\Chat\ChatController::class)->name('chat.index');
+    Route::post('/{chatId}/messages', \App\Http\Controllers\Chat\MessagePostController::class);
+});
+
+Route::prefix('/user/{userName}')->group(function () {
+    Route::get('', \App\Http\Controllers\User\UserController::class)->name('user.index');
+});
+
+Route::prefix('/user/{userName}')->middleware('auth')->group(function () {
+    Route::post('/chat', \App\Http\Controllers\Chat\MakeChatRoomController::class);
+    Route::post('/follow', \App\Http\Controllers\User\FollowAction\FollowUserController::class);
+    Route::delete('/follow', \App\Http\Controllers\User\FollowAction\UnFollowUserController::class);
+    Route::get('/follows', \App\Http\Controllers\User\FollowsController::class);
+    Route::get('/followers', \App\Http\Controllers\User\FollowersController::class);
+    Route::get('/edit', \App\Http\Controllers\User\Edit\EditController::class)->name('user.edit');
+    Route::put('/edit', \App\Http\Controllers\User\Edit\EditPutController::class)->name('user.edit.put');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
