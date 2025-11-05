cf922e6026c22e3f0ece93363195804101ed940a
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-11-05 09:09:24 +0900

    ユーザー一覧にレイアウト適用

---
+++ backend/resources/views/components/element/link-get.blade.php
@@ -0,0 +1,10 @@
+<!-- GETリクエスト用のリンクコンポーネント -->
+@props([
+    'href'=>route('quoot.index'),
+])
+
+<a 
+    href={{$href}}
+    class="py-1.5 px-4 bg-gray-50 hover:bg-gray-100 active:bg-gray-200 rounded-lg">
+        {{$slot}}
+</a>
--- backend/resources/views/user/index.blade.php
+++ backend/resources/views/user/index.blade.php
@@ -1,56 +1,51 @@
-<!doctype html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Quotter</title>
-</head>
-<body>
-    <h1>Quotter</h1>
-    <h2>ユーザ {{$userName}} のページ</h2>
-    <p>{{$profile}}</p>
-    <button onClick="location.href='/quoot'">Quoot一覧へ</button>
-    <button onClick="location.href='/user/{{$userName}}/follows'">フォローリストへ</button>
-    <button onClick="location.href='/user/{{$userName}}/followers'">フォロワーリストへ</button>
-    @if (! $isMyPage)
-        @if (! $hasFollowed)
-            <form method="POST" action="/user/{{$userName}}/follow" style="display: inline;">
-                @csrf
-                <button type="submit">フォローする</button>
-            </form>
-        @else
-            <form method="POST" action="/user/{{$userName}}/follow" style="display: inline;">
-                @csrf
-                @method('DELETE')
-                <button type="submit" onclick="return confirm('本当に解除しますか？')">フォロー解除</button>
-            </form>
-        @endif
-        <!-- チャットを開始する -->
-        <form method="POST" action="/user/{{$userName}}/chat">
-            @csrf
-            <button type="submit">チャットを開始する</button>
-        </form>
-    @else
-        <button onClick="location.href='/user/{{$userName}}/edit'">プロフィール編集</button>
-    @endif
-    @foreach ($quoots as $quoot)
-        <div style="border: 1px solid black; margin: 10px; padding: 10px;">
-            <p>Quoot ID: {{$quoot->id}}</p>
-            <p>内容: {{$quoot->content}}</p>
-            <p>作成者: {{$quoot->getDisplayName()}}</p>
-            <p>作成日: {{$quoot->created_at}}</p>
-            <!-- ログインユーザーIDの場合、修正・削除可能 -->
-            @if ($isMyPage)
-                <button onClick="location.href='/quoot/update/{{$quoot->id}}?redirect=/user/{{rawurlencode($userName)}}'">修正</button>
-                <form method="POST" action="{{ route('quoot.delete', ['quootId' => $quoot->id, 'redirect' => '/user/' . rawurlencode($userName)]) }}" style="display: inline;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" onclick="return confirm('本当に削除しますか？')">削除</button>
-                </form>
+<x-layout>
+    <x-header></x-header>
+    <x-main>
+        <div class="h-8"></div>
+        <div class="flex justify-between">
+            <div class="ml-8">
+                <h2 class="text-3xl font-bold mb-4">{{$displayName}}</h2>
+                <p>{!!nl2br(e($profile))!!}</p>
+            </div>
+            <div>
+                <ul class="flex space-x-4">
+                    <li><a href="{{route('user.follows',['userName'=>$userName])}}" class="text-center text-gray-500 hover:text-black">Follows</a></li>
+                    <li><a href="{{route('user.followers',['userName'=>$userName])}}" class="text-center text-gray-500 hover:text-black">Followers</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="flex flex-wrap justify-center">
+            @if (!$isMyPage)
+                @if(!$hasFollowed)
+                    <div>
+                        <form action="/user/{{$userName}}/follow" method="post">
+                            @csrf
+                            <x-element.button-post>フォローする</x-element.button-post>
+                        </form>
+                    </div>
+                @else
+                    <div>
+                        <form action="/user/{{$userName}}/follow" method="post">
+                            @method('DELETE')
+                            @csrf
+                            <x-element.button-post>フォロー解除</x-element.button-post>
+                        </form>
+                    </div>
+                @endif
+                <div>
+                    <form action="/user/{{$userName}}/chat" method="post">
+                        @csrf
+                        <x-element.button-post>チャットを開始</x-element.button-post>
+                    </form>
+                </div>
+            @else
+                <x-element.link-get :href="route('user.edit',['userName'=>$userName])">プロフィールを編集</x-element.link-get>
             @endif
         </div>
-    @endforeach
-</body>
-</html>
+
+        <x-quoot.list :quoots="$quoots"></x-quoot.list>
+
+    </x-main>    
+    
+</x-layout>
