e844b21701c3cb62cc312aba274c586868a7f2bf
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-11-06 12:44:49 +0900

    ユーザー一覧にプロフィール画像を表示

--- backend/resources/views/components/user/list.blade.php
+++ backend/resources/views/components/user/list.blade.php
@@ -6,8 +6,17 @@
     <ul>
         @foreach($users as $user)
             <li class="border-b last:border-0 border-gray-200 p-4">
-                <p class="text-xl"><a href="/user/{{$user->user_name}}">{{$user->display_name}}</a></p>
-                <p>{!!nl2br(e($user->profile))!!}</p>
+                <div class="flex">
+                    @if ($user->getImagePath())
+                        <img src="{{ asset('storage/' . $user->getImagePath()) }}" alt="プロフィール画像" class="w-12 h-12 rounded-full object-cover">
+                    @else
+                        <img src="{{ asset('storage/default_profile_icon.png') }}" alt="プロフィール画像" class="w-12 h-12 rounded-full object-cover">
+                    @endif
+                    <div class="ml-8">
+                        <p class="text-xl"><a href="/user/{{$user->user_name}}">{{$user->display_name}}</a></p>
+                        <p>{!!nl2br(e($user->profile))!!}</p>
+                    </div>
+                </div>
             </li>
         @endforeach
     </ul>
