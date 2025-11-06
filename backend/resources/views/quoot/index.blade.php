8da58ad57185470375cf6d9ef9f014628750dd74
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-11-06 09:43:59 +0900

    ヘッダーにログイン/ログアウトボタン追加

--- backend/resources/views/quoot/index.blade.php
+++ backend/resources/views/quoot/index.blade.php
@@ -8,8 +8,6 @@
                 <input type="hidden" name="onlyFollowees" value="{{ $onlyFollowees ? '0' : '1' }}">
                 <button type="submit">{{ $onlyFollowees ? '全てのQuootを表示' : 'フォロー中のユーザのQuootのみ表示' }}</button>
             </form>
-        @else
-            <button onClick="location.href='/login'">ログイン画面へ</button>
         @endif
         <x-quoot.list :quoots="$quoots" :onlyFollowees="$onlyFollowees"></x-quoot.list>
     </x-main>
